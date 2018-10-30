6ed55cc6d073f5fcbaed46b24a40bb26d5cfd77e
kaljarys <kbrantone@gmail.com>
2018-10-30 11:46:41 +0100

    mastermind finalement OK, debut de mastermind defensif

--- src/main/java/fr/upsy/mastermind/Mastermind.java
+++ src/main/java/fr/upsy/mastermind/Mastermind.java
@@ -29,8 +29,8 @@ public class Mastermind {
 			System.out.println("Essayez de deviner la combinaison de votre adversaire :  \n");
 			int joueurProposition = scJoueurProposition.nextInt();
 		/**reset des variables **/
-			present = 0;
 			bienPlace = 0;
+			present = 0;
 		/**transforme joueurProposition en arraylist**/
 			String[] temp = Integer.toString(joueurProposition).split("");
 			ArrayList<Integer> arrayJoueurProposition = new ArrayList<Integer>();
@@ -40,36 +40,92 @@ public class Mastermind {
 			}
 			System.out.println(arrayJoueurProposition);
 			
-		/**compare les combinaison**/
+		/*compare les combinaisons*/
 			
+			ArrayList<Integer> arrayIndex = new ArrayList<Integer>();
 			
 			for(int i = 0; i < combinaisonMachine.size(); i++) {
-				if(arrayJoueurProposition.get(i) == combinaisonMachine.get(i) ) {   /**si bien place bien place++**/
-					
-					listComb.add(arrayJoueurProposition.get(i));
+				if(arrayJoueurProposition.get(i) == combinaisonMachine.get(i) ) {   /**si bien place bienPlace++**/
+					arrayIndex.add(i);
 					
 					bienPlace ++;
 					
 				}
-				else if (combinaisonMachine.contains(arrayJoueurProposition.get(i)) ){
-					listComb.add(arrayJoueurProposition.get(i));
-					
-					present++;
-					
-					
-						}
-					
+			}
+				 
+			for(int j =0; j < combinaisonMachine.size(); j++) {
+				for(int k = 0; k < combinaisonMachine.size(); k++) {
+					if(arrayJoueurProposition.get(j) == combinaisonMachine.get(k) && !arrayIndex.contains(k) ) {
+						present++;
+						arrayIndex.add(k);
+						break;
+					}
+				}
 				
 			}
-			System.out.println(listComb);
-			/**renvoi present et bien place**/
+				
 			
-		System.out.println("Proposition : " + joueurProposition + " -> reponse : presents " + (present) + ", bien places " + bienPlace);
+		
+			
+			/**renvoi present et bien place**/
+			System.out.println(arrayIndex);
+		System.out.println("Proposition : " + joueurProposition + " -> reponse : presents " + present + ", bien places " + bienPlace);
 		
 		
 		}
 		
 		System.out.println("Bravo vous avez trouve la combinaison !!");
 	}
-
+	
+	/*public static int BienPlace(ArrayList<Integer> combinaisonMachine, ArrayList<Integer> arrayJoueurProposition) {
+			
+		int  bienPlace = 0 ;
+			for(int i = 0; i < combinaisonMachine.size(); i++) {
+				if(arrayJoueurProposition.get(i) == combinaisonMachine.get(i) ) {   *//**si bien place bienPlace++**//*
+					
+					
+					bienPlace ++;
+					
+				}
+			}
+			return bienPlace;
+					
+				
+			}
+	public static int Present(ArrayList<Integer> combinaisonMachine, ArrayList<Integer> arrayJoueurProposition) {
+		int present = 0;
+		int present1 = 0;
+		int present2 = 0;
+		int present3 = 0;
+		int present4 = 0;
+		int present5 = 0;
+		int present6 = 0;
+		int present7 = 0;
+		int present8 = 0;
+		int present9 = 0;
+		for(int i = 0; i < combinaisonMachine.size(); i++) {
+		 if (combinaisonMachine.contains(arrayJoueurProposition.get(i)) && arrayJoueurProposition.get(i) != combinaisonMachine.get(i)) {
+				present++;
+			}
+		}
+		return present;
+	}*/
+		
+	
 }
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/fr/upsy/mastermind/Resultat.java
@@ -0,0 +1,20 @@
+package fr.upsy.mastermind;
+
+public class Resultat {
+	
+	private int bienPlace;
+	public int getBienPlace() {
+		return bienPlace;
+	}
+	public void setBienPlace(int bienPlace) {
+		this.bienPlace = bienPlace;
+	}
+	public int getPresent() {
+		return present;
+	}
+	public void setPresent(int present) {
+		this.present = present;
+	}
+	private int present;
+
+}
